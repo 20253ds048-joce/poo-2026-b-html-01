3e7ec70ae776744e74b95a1c0ede19c06878050c
Jocelyn Martinez <20253ds048@utez.edu.mx>
2026-05-29 14:31:56 -0600

    [MODIFICACION] modificacion prectica

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const btnSaludar = document.getElementById("btn-saludar");
+let mensajeMostrado = false;
+
+console.log(btnSaludar);
+
+btnSaludar.addEventListener("click", function () {
+    console.log("click");
+
+    if(!mensajeMostrado){
+        const seccionContacto = document.getElementById("contacto");
+    
+        const mensajes = document.createElement("p");
+        mensajes.textContent = "Gracias por tu visita";
+        mensajes.style.color = "#27ae60";
+        mensajes.style.fontWeight = "bold";
+
+        seccionContacto.appendChild(mensajes);
+        mensajeMostrado = true;
+        btnSaludar.textContent = "Mensaje enviado";
+        btnSaludar.disabled = true;
+        btnSaludar.style.backgroundColor = "#bdc3c7";
+
+    }
+
+}   
+) 
